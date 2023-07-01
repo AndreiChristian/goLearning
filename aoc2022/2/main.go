05b5adea649b5697f3a4698e51478c763f015a26
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-01 17:31:59 +0200

    more practice with go

--- aoc2022/2/main.go
+++ aoc2022/2/main.go
@@ -1,9 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
 
 func main() {
 
-	fmt.Println("Hello world")
+	data, err := ioutil.ReadFile("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tournament := strings.Split(string(data), "\n")
+
+	score := 0
+
+	for _, games := range tournament {
+
+		if games == "" {
+			continue
+		}
+
+		choices := strings.Split(games, " ")
+
+		opponentChoice := choices[0]
+		myChoice := choices[1]
+
+		switch myChoice {
+		case "X": // Rock
+			score += 1
+			switch opponentChoice {
+			case "A": // draw
+				score += 3
+			case "B": // lose, no extra points
+			case "C": // win
+				score += 6
+			}
+		case "Y": // Paper
+			score += 2
+			switch opponentChoice {
+			case "A": // win
+				score += 6
+			case "B": // draw
+				score += 3
+			case "C": // lose, no extra points
+			}
+		case "Z": // Scissors
+			score += 3
+			switch opponentChoice {
+			case "A": // lose, no extra points
+			case "B": // win
+				score += 6
+			case "C": // draw
+				score += 3
+			}
+		}
+	}
+
+	fmt.Println(score)
 
 }
