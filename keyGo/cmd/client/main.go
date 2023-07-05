6eef78d273e2e72c34280719770dafc6d67e7da2
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-05 11:20:11 +0200

    basic functionalities of redis are implemented
    there is a server that listens to connections of a tcp server exposing port 6379 which parses the requests and executes the parsed commands based on input
    the client connects to the server and enables user to interact with the server. Further work will be, for the moment, on improving the cli

--- keyGo/cmd/client/main.go
+++ keyGo/cmd/client/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error connecting to server: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		_, err := fmt.Fprintln(conn, text)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error sending command: %v\n", err)
+			os.Exit(1)
+		}
+
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading response: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Print(response)
+	}
+}
